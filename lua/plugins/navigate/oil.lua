236c26036e7b689dc4c6125337cd40d8a2af214b
Brian Li <brianchunghli@gmail.com>
2024-01-15 20:09:06 +1100

    feature: add ability to quit with q

--- lua/plugins/navigate/oil.lua
+++ lua/plugins/navigate/oil.lua
@@ -2,14 +2,17 @@ if not vim.g.vscode then
   return {
     'stevearc/oil.nvim',
     keys = {
-      { "<leader>tt", "<cmd>lua require('oil').toggle_float('.')<cr>", desc = "Browse files for editting" },
+      { "<leader>tt", "<cmd>lua require('oil').toggle_float('.')<cr>", desc = "Browse files for editing" },
     },
     opts = {
       float = {
         padding = 10,
         max_width = 100,
         max_height = 20,
-      }
+      },
+      keymaps = {
+        ["q"] = "actions.close",
+      },
     },
     -- Optional dependencies
     dependencies = { "nvim-tree/nvim-web-devicons" },
