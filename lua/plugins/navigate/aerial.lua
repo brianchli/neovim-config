30013e472f5410b7940953e21f3428a7ebd0803f
Brian Li <brianchunghli@gmail.com>
2024-01-15 19:58:30 +1100

    chore: add which key message

--- lua/plugins/navigate/aerial.lua
+++ lua/plugins/navigate/aerial.lua
@@ -20,7 +20,7 @@ if not vim.g.vscode then
         },
       })
       -- You probably also want to set a keymap to toggle aerial
-      vim.keymap.set('n', '<leader>a', '<cmd>AerialToggle!<CR>')
+      vim.keymap.set('n', '<leader>a', '<cmd>AerialToggle!<CR>', { desc = "toggle aerial view" })
     end
   }
 end
