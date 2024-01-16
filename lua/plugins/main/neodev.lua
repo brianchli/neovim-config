7c3c8957a62b8797f07c23996c1c1fa0f74f5c92
Brian Li <brianchunghli@gmail.com>
2024-01-16 18:48:49 +1100

    chore: consolidated neodev requires

--- lua/plugins/main/neodev.lua
+++ lua/plugins/main/neodev.lua
@@ -4,7 +4,10 @@ if not vim.g.vscode then
     opts = {},
     priority = 1000,
     config = function()
-      require('neodev').setup()
+      require('neodev').setup(
+        {
+          library = { plugins = { "nvim-dap-ui" }, types = true },
+        })
     end
   }
 end
