728cc8705d7edf39b1e3991a65a15d28459643f3
Brian Li <brianchunghli@gmail.com>
2024-01-23 15:32:37 +1100

    refactor: simplify lsp config and add descriptions

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -15,46 +15,59 @@ if not vim.g.vscode then
     config = function()
       local status, lspconfig = pcall(require, 'lspconfig')
       if status then
-        local _, navic = pcall(require, 'nvim-navic')
-        local lsp_capabilities = lspconfig.util.default_config
-        lsp_capabilities.capabilities = vim.tbl_deep_extend(
-          'force',
-          lsp_capabilities.capabilities,
-          require('cmp_nvim_lsp').default_capabilities())
+        local _, navic                    = pcall(require, 'nvim-navic')
+        local lsp_capabilities            = vim.tbl_deep_extend("force",
+          vim.lsp.protocol.make_client_capabilities(),
+          require('cmp_nvim_lsp').default_capabilities()
+        )
         lsp_capabilities.update_in_insert = false
-        lsp_capabilities.flags = {
+        lsp_capabilities.flags            = {
           debounce_text_changes = 150,
           allow_incremental_sync = true,
         }
 
-        local opts = { noremap = true, silent = true }
-        vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-        vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+        vim.keymap.set('n', '[d', vim.diagnostic.goto_prev,
+          { noremap = true, silent = true, desc = "goto next diagnostic" })
+        vim.keymap.set('n', ']d', vim.diagnostic.goto_next,
+          { noremap = true, silent = true, desc = "goto prev diagnostic" })
 
         local on_attach = function(client, bufnr)
           -- Enable completion triggered by <c-x><c-o>
-          vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+          -- NOTE: Not sure if this is required? Test how it is without it for now.
+          -- vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
           -- Mappings.
           -- See `:help vim.lsp.*` for documentation on any of the below functions
-          local bufopts = { noremap = true, silent = true, buffer = bufnr }
-          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-          vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-          vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-          vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration,
+            { noremap = true, silent = true, buffer = bufnr, desc = "goto declaration" })
+          vim.keymap.set('n', 'gd', vim.lsp.buf.definition,
+            { noremap = true, silent = true, buffer = bufnr, desc = "goto definition" })
+          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation,
+            { noremap = true, silent = true, buffer = bufnr, desc = "goto implementation" })
+          vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help,
+            { noremap = true, silent = true, buffer = bufnr, desc = "signature help" })
+          vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder,
+            { noremap = true, silent = true, buffer = bufnr, desc = "add workspace folder" })
+          vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder,
+            { noremap = true, silent = true, buffer = bufnr, desc = "remove workspace folder" })
           vim.keymap.set('n', '<space>wl', function()
-            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-          end, bufopts)
-          vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-          vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-          vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-          vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+              print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+            end,
+            { noremap = true, silent = true, buffer = bufnr, desc = "list workspace folders" })
+
+          vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition,
+            { noremap = true, silent = true, buffer = bufnr, desc = "buf type definition" })
+          vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename,
+            { noremap = true, silent = true, buffer = bufnr, desc = "buf rename" })
+          vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action,
+            { noremap = true, silent = true, buffer = bufnr, desc = "code actions" })
+          vim.keymap.set('n', '<space>F', function() vim.lsp.buf.format { async = true } end,
+            { noremap = true, silent = true, buffer = bufnr, desc = "format file" })
 
           if client.server_capabilities.documentSymbolProvider then
             navic.attach(client, bufnr)
           end
+
           require("clangd_extensions.inlay_hints").setup_autocmd()
           require("clangd_extensions.inlay_hints").set_inlay_hints()
         end
