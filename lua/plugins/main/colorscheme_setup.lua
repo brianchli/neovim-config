90c4a9ec1aab7a1cfcde66a7915a8f6fbfea8a58
Brian Li <brianchunghli@gmail.com>
2024-05-08 10:40:00 +1000

    chore: new colorscheme (woo)

--- lua/plugins/main/colorscheme_setup.lua
+++ lua/plugins/main/colorscheme_setup.lua
@@ -1,35 +1,63 @@
-return {
-  {
-    'catppuccin/nvim',
-    name = 'catppuccin',
-    config = function()
-      require('catppuccin').setup({
-        flavour = 'macchiato',
-        dim_inactive = {
-          enabled = false,
-          shade = "dark",
-          percentage = 0.15,
-        },
-        integrations = {
-          cmp = true,
-          gitsigns = true,
-          nvimtree = true,
-          telescope = true,
-          notify = false,
-        },
-      })
+--return {
+--  {
+--    'catppuccin/nvim',
+--    name = 'catppuccin',
+--    config = function()
+--      require('catppuccin').setup({
+--        flavour = 'macchiato',
+--        dim_inactive = {
+--          enabled = false,
+--          shade = "dark",
+--          percentage = 0.15,
+--        },
+--        integrations = {
+--          cmp = true,
+--          gitsigns = true,
+--          nvimtree = true,
+--          telescope = true,
+--          notify = false,
+--        },
+--      })
+--
+--      local cmd = vim.cmd
+--      vim.o.termguicolors = true
+--      cmd.colorscheme 'catppuccin'
+--      local macchiato = require("catppuccin.palettes").get_palette "macchiato"
+--      -- setting the outlines for highlighting the current line
+--      vim.api.nvim_set_hl(
+--        0, 'Cursor', { bg = macchiato.surface0, fg = macchiato.text }
+--      )
+--      vim.api.nvim_set_hl(
+--        0, 'CurSearch', { bg = macchiato.surface1, fg = macchiato.text })
+--    end,
+--    priority = 1000
+--  },
+--}
 
-      local cmd = vim.cmd
-      vim.o.termguicolors = true
-      cmd.colorscheme 'catppuccin'
-      local macchiato = require("catppuccin.palettes").get_palette "macchiato"
-      -- setting the outlines for highlighting the current line
-      vim.api.nvim_set_hl(
-        0, 'Cursor', { bg = macchiato.surface0, fg = macchiato.text }
-      )
-      vim.api.nvim_set_hl(
-        0, 'CurSearch', { bg = macchiato.surface1, fg = macchiato.text })
-    end,
-    priority = 1000
-  },
+return {
+  'navarasu/onedark.nvim',
+  name = 'onedark',
+  lazy = false,
+  priority = 1000,
+  dependencies = { "catppuccin/nvim" },
+  config = function()
+    require('catppuccin').setup({
+      flavour = 'macchiato',
+      dim_inactive = {
+        enabled = false,
+        shade = "dark",
+        percentage = 0.15,
+      },
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        telescope = true,
+        notify = false,
+      },
+    })
+    local cmd = vim.cmd
+    vim.o.termguicolors = true
+    cmd.colorscheme 'onedark'
+  end
 }
