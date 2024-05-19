ffcbebe8a41a16ec04d30ceb1aac7238b98b87f7
Brian Li <brianchunghli@gmail.com>
2024-05-19 12:30:14 +1000

    refactor: moved configuration to opt key field

--- lua/plugins/navigate/vim-tmux-navigator.lua
+++ lua/plugins/navigate/vim-tmux-navigator.lua
@@ -2,17 +2,18 @@
 if not vim.g.vscode then
   return {
     "alexghergh/nvim-tmux-navigation",
-    config = function()
-      require('nvim-tmux-navigation').setup {
-        keybindings = {
-          left = "<C-h>",
-          down = "<C-j>",
-          up = "<C-k>",
-          right = "<C-l>",
-          last_active = "<C-\\>",
-          next = "<C-Space>",
-        }
+    opts = {
+      keybindings = {
+        left = "<C-h>",
+        down = "<C-j>",
+        up = "<C-k>",
+        right = "<C-l>",
+        last_active = "<C-\\>",
+        next = "<C-Space>",
       }
+    },
+    config = function(_, opts)
+      require('nvim-tmux-navigation').setup(opts)
     end
   }
 end
