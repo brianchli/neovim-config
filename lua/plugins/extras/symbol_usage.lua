16167eb25b3ad586a7b53fcc5dbc577fc5122e8b
Brian Li <brianchunghli@gmail.com>
2024-05-21 15:00:45 +1000

    refactor: added symbol usage autocommand

--- lua/plugins/extras/symbol_usage.lua
+++ lua/plugins/extras/symbol_usage.lua
@@ -1,9 +1,23 @@
 if not vim.g.vscode then
   return {
     'Wansmer/symbol-usage.nvim',
-    event = 'BufReadPre', -- need run before LspAttach if you use nvim 0.9. On 0.10 use 'LspAttach'
+    event = 'LspAttach',
     config = function()
       require('symbol-usage').setup()
+
+      vim.api.nvim_create_autocmd({ "InsertLeave", }, {
+        group = augroup('autoupdate-symbol-usage'),
+        desc = "updates symbols upon leaving insert mode",
+        pattern = {
+          "cpp",
+          "py",
+          "c",
+          "lua"
+        },
+        callback = function()
+          vim.fn('lua require("symbol-usage").refresh()"')
+        end
+      })
     end
   }
 end
