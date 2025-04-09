8394b0b8bbc9c44b62b9de42a155e2ad5e4356cd
Brian Li <brianchunghli@gmail.com>
2025-04-09 18:31:30 +1000

    refactor: remove smooth scrolling for page up and down

--- lua/plugins/ui/snacks.lua
+++ lua/plugins/ui/snacks.lua
@@ -15,7 +15,6 @@ if not vim.g.vscode then
       notifier = { enabled = true },
       quickfile = { enabled = true },
       scope = { enabled = true },
-      scroll = { enabled = true },
       statuscolumn = { enabled = true },
       words = { enabled = true },
     },
