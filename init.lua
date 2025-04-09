80e19c055882b49e140ace0fe8f74756c3c574a9
Brian Li <brianchunghli@gmail.com>
2025-04-09 12:55:30 +1000

    refactor: move lazy.nvim setup

--- init.lua
+++ init.lua
@@ -19,5 +19,38 @@ if not vim.g.vscode then
   require('conf.opts')      -- options
   require('conf.keymaps')   -- key mappings
   require('conf.autofuncs') -- functions and autocmds
-  require('conf.setup')     -- plugins
+
+  local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+  if not vim.uv.fs_stat(lazypath) then
+    vim.fn.system({
+      'git',
+      'clone',
+      '--filter=blob:none',
+      'https://github.com/folke/lazy.nvim.git',
+      '--branch=stable', -- latest stable release
+      lazypath,
+    })
+  end
+  vim.opt.rtp:prepend(lazypath)
+  local status, lazy = pcall(require, 'lazy')
+  if status then
+    lazy.setup({
+      spec = {
+        { import = 'plugins.main' },
+        { import = 'plugins.ui' },
+        { import = 'plugins.lang-support' },
+        { import = 'plugins.styling' },
+        { import = 'plugins.navigation' },
+        { import = 'plugins.extras' },
+        { import = 'plugins.diagnostics' },
+      },
+      checker = {
+        enabled = true,
+        notify = false,
+      },
+      change_detection = {
+        notify = false
+      }
+    })
+  end
 end
