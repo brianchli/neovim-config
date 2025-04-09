6991de718cc963444d56cf4e1403d808f7008c59
Brian Li <brianchunghli@gmail.com>
2025-04-09 18:31:08 +1000

    chore: remove unneeded features

--- lua/plugins/extras/vimbegood.lua
+++
@@ -1,9 +0,0 @@
-if not vim.g.vscode then
-  return {
-    'ThePrimeagen/vim-be-good',
-    keys = {
-      { "<leader>pv", "<cmd>VimBeGood<cr>", desc = "play vim be good" },
-    },
-    lazy = 'VeryLazy'
-  }
-end
