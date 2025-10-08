0294c5f7a00dd71dd2ca464417301f0b65163ca2
Brian Li <brianchunghli@gmail.com>
2025-10-08 22:00:57 +1100

    chore: run formatter

--- lua/plugins/ui/colorschemes.lua
+++ lua/plugins/ui/colorschemes.lua
@@ -1,9 +1,9 @@
 -- load colorschemes
 return {
-  { 'Mofiqul/dracula.nvim',          name = 'dracula',  lazy = false },
-  { 'drewtempelmeyer/palenight.vim', name = 'pale',     lazy = false },
-  { 'rebelot/kanagawa.nvim',         name = 'kanagawa', lazy = false },
-  { 'bluz71/vim-nightfly-colors',    name = 'nightfly', lazy = false },
+  { 'Mofiqul/dracula.nvim',            name = 'dracula',    lazy = false },
+  { 'drewtempelmeyer/palenight.vim',   name = 'pale',       lazy = false },
+  { 'rebelot/kanagawa.nvim',           name = 'kanagawa',   lazy = false },
+  { 'bluz71/vim-nightfly-colors',      name = 'nightfly',   lazy = false },
   { 'olivercederborg/poimandres.nvim', name = 'poimandres', lazy = false },
   { 'Mofiqul/vscode.nvim',             name = 'vscode',     lazy = false },
   { 'p00f/alabaster.nvim',             name = 'alabaster',  lazy = false },
