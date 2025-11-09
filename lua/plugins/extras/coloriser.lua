ad2f7f4be051bbb1c24a2a552c49810333365be6
Brian Li <brianchunghli@gmail.com>
2025-11-10 09:13:33 +1100

    update colorizer config

--- lua/plugins/extras/coloriser.lua
+++ lua/plugins/extras/coloriser.lua
@@ -1,14 +1,14 @@
 if not vim.g.vscode then
   return {
     'norcalli/nvim-colorizer.lua',
-    keys = {
-      { "<leader>cs", "<cmd>ColorizerToggle<cr>", desc = "colorise hex codes" },
-    },
     priority = 200,
     config = function()
       local status, colorizer = pcall(require, 'colorizer')
       if status then
-        colorizer.setup()
+        colorizer.setup({
+          "*", css = { rgb_fn = true }
+
+        })
       end
     end
   }
