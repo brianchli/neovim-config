f9281bb70976e02827840988c27918a5f93d5fb7
Brian Li <brianchunghli@gmail.com>
2025-11-15 10:48:02 +1100

    refactor: update identation formatting and remove rainbow delimiters

--- lua/plugins/qol/indent-blankline.lua
+++ lua/plugins/qol/indent-blankline.lua
@@ -3,18 +3,23 @@ if not vim.g.vscode then
     'lukas-reineke/indent-blankline.nvim',
     main = "ibl",
     opts = {
-      space_char_blankline = " ",
+      space_char_blankline = "",
       show_current_context = true,
     },
     config = function()
-      require("ibl").setup(
-        {
-          scope = {
-            show_start = false,
-          }
-
-        }
-      )
+      local highlight = {
+        "NonText",
+      }
+      require("ibl").setup {
+        indent = { highlight = highlight, smart_indent_cap = true, char = "|" },
+        whitespace = {
+          highlight = highlight,
+          remove_blankline_trail = false,
+        },
+        scope = {
+          show_start = false
+        },
+      }
     end
   }
 end
